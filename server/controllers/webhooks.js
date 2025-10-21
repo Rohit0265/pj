9482f3cb45e61d37931803a59dbb47fdb0b0421b
Rohit Mathur <116487107+Rohit0265@users.noreply.github.com>
2025-10-21 23:51:07 +0530

    op

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,19 +1,19 @@
 import { Webhook } from "svix";
 import user from "../model/user.js";
 
-//api clerk 
-
-const clerkwebhooks = async(requestAnimationFrame, res) => {
+// api clerk
+const clerkwebhooks = async(req, res) => { // fixed parameter name
     try {
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
+        // verify the webhook
         await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
             "svix-timestamp": req.headers["svix-timestamp"],
             "svix-signature": req.headers["svix-signature"]
-        })
+        });
 
-        const { data, type } = req.body
+        const { data, type } = req.body;
 
         switch (type) {
             case 'user.created':
@@ -23,9 +23,9 @@ const clerkwebhooks = async(requestAnimationFrame, res) => {
                         email: data.email_addresses[0].email_address,
                         name: data.first_name + " " + data.last_name,
                         imageUrl: data.image_url,
-                    }
-                    await user.create(userdata)
-                    res.json({})
+                    };
+                    await user.create(userdata);
+                    res.json({ success: true }); // added success flag
                     break;
                 }
             case 'user.updated':
@@ -34,24 +34,27 @@ const clerkwebhooks = async(requestAnimationFrame, res) => {
                         email: data.email_addresses[0].email_address,
                         name: data.first_name + " " + data.last_name,
                         imageUrl: data.image_url,
-                    }
-                    await user.findByIdAndUpdate(data.id, userdata)
-                    res.json({})
+                    };
+                    await user.findByIdAndUpdate(data.id, userdata);
+                    res.json({ success: true });
                     break;
                 }
             case 'user.deleted':
                 {
-                    await user.findByIdAndDelete(data.id)
-                    res.json({})
+                    await user.findByIdAndDelete(data.id);
+                    res.json({ success: true });
                     break;
                 }
             default:
+                res.json({ success: true, message: "Unhandled event" }); // added default response
                 break;
         }
     } catch (error) {
         res.json({
             success: false,
             message: error.message
-        })
+        });
     }
-}
+};
+
+export default clerkwebhooks;
