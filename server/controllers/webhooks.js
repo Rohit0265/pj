23e12944c23c36df9091795becff87a081462629
Rohit Mathur <116487107+Rohit0265@users.noreply.github.com>
2025-10-22 23:07:15 +0530

    hello

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,13 +1,14 @@
 import { Webhook } from "svix";
-import User from "../model/user.js"; // Capitalized for clarity, but must match file
+import User from "../model/user.js"; // Must match file
 
 const clerkwebhooks = async(req, res) => {
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
+        // Commented out timestamp for local testing
         await whook.verify(JSON.stringify(req.body), {
             "svix-id": req.headers["svix-id"],
-            // "svix-timestamp": req.headers["svix-timestamp"],
+            // "svix-timestamp": req.headers["svix-timestamp"], 
             "svix-signature": req.headers["svix-signature"]
         });
 
